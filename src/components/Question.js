0034f987a160650e4e9f2c9f9169d887dbe8ff49
Jahdon Ervin <Jahdonervin@gmail.com>
2023-03-23 18:55:31 -0400

    commit tree 9131426fc384bf9d2558b167f49a65a3c37d5dd8

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+   
+   useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return; 
+    }
 
-  // add useEffect code
+    const timerId = setTimeout(() => {
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
 
-  function handleAnswer(isCorrect) {
+    return function () {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered]);
+
+function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
-
   const { id, prompt, answers, correctIndex } = question;
-
   return (
     <>
       <h1>Question {id}</h1>
